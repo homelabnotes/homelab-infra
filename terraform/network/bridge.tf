267cbf329cafd4e1d3bf15c1904db7c6694638a6
HomelabNotes <woitzikunlimited@gmail.com>
2025-09-28 01:10:56 +0200

    Change to working Trunk Ports :)

--- terraform/network/bridge.tf
+++ terraform/network/bridge.tf
@@ -31,7 +31,7 @@ resource "routeros_interface_bridge_port" "bridge_ports" {
     "ether4"       = { comment = "", pvid = "1" }
     "ether5"       = { comment = "MGMT", pvid = "1" }
     "ether6"       = { comment = "", pvid = "1" }
-    "ether7"       = { comment = "PROD", pvid = "20" }
+    "ether7"       = { comment = "TRUNK", pvid = "20" }
     "ether8"       = { comment = "TRUNK", pvid = "20" }
     "sfp-sfpplus1" = { comment = "", pvid = "1" }
   }
